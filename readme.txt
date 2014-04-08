fbb505fd1248f7013e388697ea1e0602cc6963b9
cdillon27 <cdillon27@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-08 10:00:55 +0000

    Adding v0.2 to svn trunk.
    git-svn-id: https://plugins.svn.wordpress.org/wider-admin-menu/trunk@889276 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,93 @@
+=== Wider Admin Menu ===
+Contributors: cdillon27
+Donate link: http://www.wpmission.com/donate/
+Tags: admin menu
+Requires at least: 3.3
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adjust the width of the Admin Menu to accomodate long menu items.
+
+== Description ==
+
+No more line breaks on long menu items! Got a wide screen? Let's use it!
+
+Easily set the width of the Admin Menu from the default `160px` up to `300px` in increments of 10.
+
+When I say easy, I mean *easy*. Like drag a slider and click `Save`. You can manually enter the width too. Pretty hard to screw up.
+
+This plugin adds your width setting to a small `<style>` section in each admin page. A more efficient alternative is to use the separate CSS file `wider-admin-menu.css` in your theme instead of this plugin. See FAQ below.
+
+Wider Admin Menu has been tested on WordPress versions 3.3 to 3.8.1.
+
+This plugin will *leave no trace!* If you delete the plugin, all settings will be removed from the database. Guaranteed. However, simply deactivating it will leave your settings in place, as expected.
+
+== Installation ==
+
+1. Upload the `wider-admin-menu` folder to your `/wp-content/plugins/` directory
+1. Activate the plugin through the `Plugins` menu in WordPress
+1. Go to the `Wider Admin Menu` page on the `Settings` menu.
+1. Smile with joy at the power of code.
+
+== Frequently Asked Questions ==
+
+= I don't need a plugin. Can I just add some code to my stylesheet? =
+
+Sure. That's actually how this plugin started out. And if you simply need a wider admin bar and you're cool with CSS then try this: 
+
+Look in the plugin's `/css` folder for the file `wider-admin-menu.css`.
+
+Then either copy its contents to your theme's stylesheet,
+
+OR
+
+copy the file to your theme folder and add this to your theme's `functions.php` to load it:
+
+`
+function wpmwam_style() 
+{
+	wp_enqueue_style( 'wpmwam-style',	get_stylesheet_directory_uri() . '/wider-admin-menu.css' );
+}
+add_action( 'admin_enqueue_scripts', 'wpmwam_style' );
+`
+
+That covers WordPress 3.8 and up. For WordPress 3.3 to 3.7.1, substitute `wider-admin-menu-33.css`.
+
+Need a hand? I'm on the support forum.
+
+= Leave no trace? What's that about? =
+
+I'm sorry to be the one to tell you this but some plugins and themes don't fully uninstall everything they installed; things like settings, database tables, subdirectories. That bugs me. Sometimes, it bugs your WordPress too. (See what I did there?) For the most part, it's harmless but my thinking is "Why risk it? Why not clean up after yourself?" 
+
+Perhaps the best reason for leaving no trace is to make it easier to debug later. As a WordPress mechanic, I have wasted time tracking down leftover settings just to rule them out as suspects because their plugin or theme was removed. Plugins are like the kitchen and the bathroom: it's easier to keep things clean if you keep 'em clean, know what I mean?
+
+So this plugin will completely remove itself upon deletion. Deactivating the plugin will leave the settings intact, though. As an added bonus, you can switch off "Leave No Trace" so the settings remain after deletion, if you want.
+
+= Where can I submit feature requests? =
+
+On the support forum. If it's doable and adds value and others agree, I'll get right on it. If not, I'll let you know straight up so we don't waste any time.
+
+= I need a WordPress mechanic. / I have a plugin idea. =
+
+[Let's talk](http://www.wpmission.com/contact/).
+
+== Screenshots ==
+
+1. Before: 160px default width.
+2. After: the plugin sets the initial width to 200px.
+3. The sweet & simple settings page where **you** can set the width from 160px - 300px.
+
+== Translations ==
+
+Can you help? Contact me [here](http://www.wpmission.com/contact/).
+
+== Changelog ==
+
+= 0.1 =
+* First release.
+
+== Upgrade Notice ==
+
+This is the first release.
---
+++ wider-admin-menu.php
@@ -0,0 +1,302 @@
+<?php
+/*
+	Plugin Name: Wider Admin Menu
+	Description: Adjust the width of the Admin Menu to accomodate long menu items.
+	Author: Chris Dillon
+	Version: 0.2
+	Author URI: http://wpmission.com
+	Text Domain: wpmission
+	License: GPL2
+
+  Copyright 2014 • Chris Dillon • chris@wpmission.com
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+	Install with default setting.
+*/
+function wpmwam_install()
+{
+	$options = array(
+			'wpmwam_width' => 200,
+			'wpmwam_lnt' => 1
+	);
+	update_option( 'wpmwam_options', $options );
+}
+register_activation_hook( __FILE__, 'wpmwam_install' );
+
+
+/*
+	Uninstall and leave no trace.
+*/ 
+function wpmwam_uninstall()
+{
+	$options = get_option( 'wpmwam_options' );
+	if( $options['wpmwam_lnt'] )
+		delete_option( 'wpmwam_options' );
+}
+register_uninstall_hook( __FILE__, 'wpmwam_uninstall' );
+
+
+/*
+	Localization
+*/
+function wpmwam_init() 
+{
+	load_plugin_textdomain( 'wpmwam', false, 
+	plugin_basename( dirname( __FILE__ ) . '/localization' ) );
+}
+add_action( 'init', 'wpmwam_init' );
+
+
+/*
+	Load styles and scripts.
+*/
+function wpmwam_admin_style( $hook ) 
+{
+	// Are we on our settings page?
+	if( 'settings_page_wider-admin-menu' == $hook )
+	{
+		wp_enqueue_style( 'nouislider-style',	plugins_url( '/css/jquery.nouislider.min.css', __FILE__ ) );
+		wp_register_script( 'nouislider', plugins_url( '/js/jquery.nouislider.min.js', __FILE__ ), array( 'jquery' ) );
+		wp_enqueue_script( 'nouislider' );
+		
+		wp_enqueue_style( 'wpmwam-options',	plugins_url( '/css/options.css', __FILE__ ) );
+		wp_register_script( 'wpmwam-script', plugins_url( '/js/wider-admin-menu.js', __FILE__ ), array( 'nouislider' ) );
+		wp_enqueue_script( 'wpmwam-script' );
+	}
+}
+add_action( 'admin_enqueue_scripts', 'wpmwam_admin_style' );
+
+
+/*
+	Insert custom admin style.
+*/
+function wpmwam_custom_admin_style()
+{
+	$wp_version = get_bloginfo( 'version' );
+	$ver_parts = explode( '.', $wp_version );
+	$major = (int) $ver_parts[0];
+	$minor = (int) $ver_parts[1];
+	$patch = isset( $ver_parts[2] ) ? (int) $ver_parts[2] : 0;
+	
+	// Get width option. Prevent zero in case of installation error.
+	$wpmwam = get_option( 'wpmwam_options' );
+	$w = (int) $wpmwam['wpmwam_width'];
+	if( !$w ) $w = 160;
+	$wpx = $w . 'px';
+	$w1px = ( $w + 1 ) . 'px';
+	$w2px = ( $w + 20 ) . 'px';
+
+	// start output buffer
+	ob_start();
+	?>
+<style>
+	/* Wider Admin Menu for WordPress <?php echo $wp_version; ?> */
+<?php if( $major >= 3 && $minor >= 8 ) : ?>
+	#wpcontent,
+	#wpfooter {
+		margin-left: <?php echo $w2px; ?>;
+	}
+	#adminmenuback,
+	#adminmenuwrap,
+	#adminmenu,
+	#adminmenu .wp-submenu {
+		width: <?php echo $wpx; ?>;
+	}
+	#adminmenu .wp-submenu {
+		left: <?php echo $wpx; ?>;
+	}
+	#adminmenu .wp-not-current-submenu .wp-submenu,
+	.folded #adminmenu .wp-has-current-submenu .wp-submenu {
+		min-width: <?php echo $wpx; ?>;
+	}
+<?php else : ?>
+	#wpcontent,
+	#footer {
+		margin-left: <?php echo $w2px; ?>;
+	}
+	#adminmenuback,
+	#adminmenuwrap,
+	#adminmenu,
+	#adminmenu .wp-submenu,
+	#adminmenu .wp-submenu-wrap,
+	.folded #adminmenu .wp-has-current-submenu .wp-submenu {
+		width: <?php echo $wpx; ?>;
+	}
+	#adminmenu li .wp-submenu,
+	.folded #adminmenu .wp-has-current-submenu .wp-submenu {
+		left: <?php echo $w1px; ?>;
+	}
+	.wp-menu-arrow {
+		-moz-transform:    translate( <?php echo $w1px; ?> );
+		-webkit-transform: translate( <?php echo $w1px; ?> );
+		-o-transform:      translate( <?php echo $w1px; ?> );
+		-ms-transform:     translate( <?php echo $w1px; ?> );
+		transform:         translate( <?php echo $w1px; ?> );
+	}
+	#adminmenu li.wp-not-current-submenu .wp-menu-arrow {
+		-moz-transform:    translate( <?php echo $wpx; ?> );
+		-webkit-transform: translate( <?php echo $wpx; ?> );
+		-o-transform:      translate( <?php echo $wpx; ?> );
+		-ms-transform:     translate( <?php echo $wpx; ?> );
+		transform:         translate( <?php echo $wpx; ?> );
+	}
+<?php endif; ?>
+</style>
+	<?php
+	$output = ob_get_contents();
+	ob_end_clean();
+	echo $output;
+}
+add_action( 'admin_head', 'wpmwam_custom_admin_style' );
+
+
+/*
+	Add options page to Settings menu.
+*/
+function wpmwam_add_options_page() 
+{
+	add_options_page( 'Wider Admin Menu', 'Wider Admin Menu', 'manage_options', basename( __FILE__ ), 'wpmwam_page' );
+	add_action( 'admin_init', 'wpmwam_register_settings' );
+}
+add_action( 'admin_menu', 'wpmwam_add_options_page' );
+
+
+/*
+	Register the setting.
+*/
+function wpmwam_register_settings()
+{
+	register_setting( 'wpmwam_settings_group', 'wpmwam_options', 'wpmwam_sanitize_options' );
+}
+
+
+/*
+	Sanitize user input.
+	If options are stored in an array, each element must be treated separately.
+*/
+function wpmwam_sanitize_options( $input )
+{
+	$input['wpmwam_width'] = sanitize_text_field( $input['wpmwam_width'] );
+	
+	// checkbox
+	if( isset( $input['wpmwam_lnt'] ) )
+		$input['wpmwam_lnt'] = 1;
+	else
+		$input['wpmwam_lnt'] = 0;
+		
+	return $input;
+}
+
+
+/*
+	The options page.
+*/
+function wpmwam_page() 
+{
+	if( !current_user_can( 'manage_options' ) )
+		return false;
+		
+	?>
+	<div class="wrap">
+	
+		<h2><?php _e( 'Wider Admin Menu', 'wpmwam' ); ?></h2>
+		
+		<p><?php _e( 'Adjust the width of the Admin Menu to accomodate longer menu items.', 'wpmwam' ); ?></p>
+		
+		<form method="post" action="options.php">
+		
+			<?php
+			// version-based CSS classes
+			$wp_version = get_bloginfo( 'version' );
+			$ver_parts = explode( '.', $wp_version );
+			$major = (int) $ver_parts[0];
+			$minor = (int) $ver_parts[1];
+			$patch = isset( $ver_parts[2] ) ? (int) $ver_parts[2] : 0;
+			if( $major >= 3 && $minor >= 8 )
+			{
+				$version_class = 'ver38';
+				$reset_class = 'dashicons dashicons-undo';
+			}
+			else
+			{
+				$version_class = 'pre-ver38';
+				$reset_class = 'undo';
+			}
+			
+			$default_width = 160;
+			settings_fields( 'wpmwam_settings_group' );
+			$wpmwam_options = get_option( 'wpmwam_options' );
+			$wpmwam_width = $wpmwam_options['wpmwam_width'];
+			if( !$wpmwam_width ) $wpmwam_width = $default_width;
+			?>
+			
+			<input type="hidden" name="wp_version" value="<?php echo $wp_version; ?>">
+			
+			<table class="form-table wpmwam" style="width: auto;">
+				<tr>
+					<td>New</td>
+					<td class="input">
+						<input id="wpmwam_width" type="text" name="wpmwam_options[wpmwam_width]" 
+								value="<?php esc_attr_e( $wpmwam_width ); ?>" 
+								size="8" maxlength="8" tabindex="1">px
+					</td>
+					<td class="slider">
+						<div id="nouislider-wrap">
+							<div id="nouislider"></div>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>Current</td>
+					<td><span id="wpmwam_current" class="box <?php echo $version_class; ?>"><?php esc_attr_e( $wpmwam_width ); ?></span>px</td>
+					<td class="lefted">
+						<a id="reset-current" class="<?php echo $reset_class; ?>" href="#" title="<?php _e( 'revert to the current width', 'wpmwam' ); ?>"></a>
+					</td>
+				</tr>
+				<tr>
+					<td>Default</td>
+					<td><span id="wpmwam_default" class="box <?php echo $version_class; ?>"><?php echo $default_width; ?></span>px</td>
+					<td class="lefted">
+							<a id="reset-default" class="<?php echo $reset_class; ?>" href="#" title="<?php _e( 'restore default width', 'wpmwam' ); ?>"></a>
+					</td>
+				</tr>
+			</table>
+			
+			<div class="option leave-no-trace">
+				<div class="onoffswitch">
+					<input id="myonoffswitch" type="checkbox" name="wpmwam_options[wpmwam_lnt]" 
+							class="onoffswitch-checkbox" value="1" <?php checked( 1, $wpmwam_options['wpmwam_lnt'] ); ?>>
+					<label class="onoffswitch-label" for="myonoffswitch">
+						<div class="onoffswitch-inner"></div>
+						<div class="onoffswitch-switch"></div>
+					</label>
+				</div>
+				<label for="myonoffswitch"><div class="option-label"><?php _e( 'Leave No Trace', 'wpmwam' ); ?></div></label>
+				<div class="option-desc">
+					<?php _e( 'Deleting this plugin will also delete these settings.<br>Deactivating it will <b>not</b> delete these settings.', 'wpmwam' ); ?>
+				</div>
+			</div>
+			
+			<p class="submit">
+				<input type="submit" class="button-primary" value="<?php _e( 'Save', 'wpmwam' ); ?>" tabindex="2">
+			</p>
+			
+		</form>
+		
+	</div>
+	<?php
+}
